4749bb400656ed035070a80b109c07ccfb5adeae
jasserlouvi <149113403+jasserlouvi@users.noreply.github.com>
2025-02-17 20:13:44 -0800

    Update pixeleditor.lua
    removed the unnecessary for loop.

--- pixeleditor.lua
+++ pixeleditor.lua
@@ -17,28 +17,26 @@ end
 actor:BindToMessage("renderpixel", renderpixel)
 
 while task.wait(1 / rastersettings.refreshrate) do
-	for i = 10, #rowcolors, 10 do
-		folder[i].UIGradient.Color = ColorSequence.new({
-			ColorSequenceKeypoint.new(0, rowcolors[i - 9]),
-			ColorSequenceKeypoint.new(0.1, rowcolors[i - 9]),
-			ColorSequenceKeypoint.new(0.101, rowcolors[i - 8]),
-			ColorSequenceKeypoint.new(0.2, rowcolors[i - 8]),
-			ColorSequenceKeypoint.new(0.201, rowcolors[i - 7]),
-			ColorSequenceKeypoint.new(0.3, rowcolors[i - 7]),
-			ColorSequenceKeypoint.new(0.301, rowcolors[i - 6]),
-			ColorSequenceKeypoint.new(0.4, rowcolors[i - 6]),
-			ColorSequenceKeypoint.new(0.401, rowcolors[i - 5]),
-			ColorSequenceKeypoint.new(0.5, rowcolors[i - 5]),
-			ColorSequenceKeypoint.new(0.501, rowcolors[i - 4]),
-			ColorSequenceKeypoint.new(0.6, rowcolors[i - 4]),
-			ColorSequenceKeypoint.new(0.601, rowcolors[i - 3]),
-			ColorSequenceKeypoint.new(0.7, rowcolors[i - 3]),
-			ColorSequenceKeypoint.new(0.701, rowcolors[i - 2]),
-			ColorSequenceKeypoint.new(0.8, rowcolors[i - 2]),
-			ColorSequenceKeypoint.new(0.801, rowcolors[i - 1]),
-			ColorSequenceKeypoint.new(0.9, rowcolors[i - 1]),
-			ColorSequenceKeypoint.new(0.901, rowcolors[i]),
-			ColorSequenceKeypoint.new(1, rowcolors[i])
-		})
-	end
+	folder[i].UIGradient.Color = ColorSequence.new({
+		ColorSequenceKeypoint.new(0, rowcolors[1]),
+		ColorSequenceKeypoint.new(0.1, rowcolors[1]),
+		ColorSequenceKeypoint.new(0.101, rowcolors[2]),
+		ColorSequenceKeypoint.new(0.2, rowcolors[2]),
+		ColorSequenceKeypoint.new(0.201, rowcolors[3]),
+		ColorSequenceKeypoint.new(0.3, rowcolors[3]),
+		ColorSequenceKeypoint.new(0.301, rowcolors[4]),
+		ColorSequenceKeypoint.new(0.4, rowcolors[4]),
+		ColorSequenceKeypoint.new(0.401, rowcolors[5]),
+		ColorSequenceKeypoint.new(0.5, rowcolors[5]),
+		ColorSequenceKeypoint.new(0.501, rowcolors[6]),
+		ColorSequenceKeypoint.new(0.6, rowcolors[6]),
+		ColorSequenceKeypoint.new(0.601, rowcolors[7]),
+		ColorSequenceKeypoint.new(0.7, rowcolors[7]),
+		ColorSequenceKeypoint.new(0.701, rowcolors[8]),
+		ColorSequenceKeypoint.new(0.8, rowcolors[8]),
+		ColorSequenceKeypoint.new(0.801, rowcolors[9]),
+		ColorSequenceKeypoint.new(0.9, rowcolors[9]),
+		ColorSequenceKeypoint.new(0.901, rowcolors[10]),
+		ColorSequenceKeypoint.new(1, rowcolors[10])
+	})
 end
